6457a6da500d707cff2076943c039116d4bf5aba
Nardo Michele <nardom@pascal.local>
2025-05-07 09:43:05 +0200

    menù aggiornato

--- progetto/main.cpp
+++ progetto/main.cpp
@@ -2,13 +2,17 @@
 using namespace std;
 void stampamenu() {
     cout << "*************************************************\n";
-    cout << "* I - caricamento dei dati                      *\n";
-    cout << "* P - visualizzazione                           *\n";
-    cout << "* M - ricerca biglietti per spettacolo          *\n";
-    cout << "* Z - inserimento nuovi biglietti per spettacolo*\n";
-    cout << "* H - cancellazione biglietti                   *\n";
-    cout << "* L - salvataggio dei dati                      *\n";
-    cout << "* X - Esci                                      *\n";
+    cout << "* 0 - carica dati da file                       *\n";
+    cout << "* 1 - cerca corsi di uno studente(matricola)    *\n";
+    cout << "* 2 - cerca corsi di uno studente(cognome)      *\n";
+    cout << "* 3 - elenca studenti iscritti ad un corso      *\n";
+    cout << "* 4 - stampa dati di un esame di un corso       *\n";
+    cout << "* 5 - numero di studenti per corso              *\n";
+    cout << "* 6 - numero di materie per corso               *\n";
+    cout << "* 7 - cerca materie per descrizione             *\n";
+    cout << "* 8 - inserisci nuovo studente                  *\n";
+    cout << "* 9 - salva dati su file                        *\n";
+    cout << "* X - esci                                      *\n";
     cout << "*************************************************\n";
 }
 int main()
@@ -22,49 +26,31 @@ int main()
 
         switch (ch) {
 
-            case 'I':
-                carica1(head);
-                carica2(head2);
+            case '1':
                 break;
 
-            case 'P':
-                stampa(head);
-                stampa2(head2);
+            case '2':
                 break;
 
-            case 'M':
-                cout<<"inserisci lo spettacolo da cercare"<<endl;
-                cin>>s;
-                ricerca_per_spettacolo(head2,s);
+            case '3':
                 break;
 
-            case 'Z':
-                cout<<"inserisci la nuova data"<<endl;
-                cin>>data;
-                cout<<"inserisci il nuovo codice"<<endl;
-                cin>>codice;
-                cout<<"inserisci la nuova fila"<<endl;
-                cin>>fila;
-                cout<<"inserisci il nuovo posto"<<endl;
-                cin>>posto;
-                cout<<"inserisci il nuovo prezzo"<<endl;
-                cin>>prezzo;
-                cout<<"inserisci il titolo"<<endl;
-                cin>>titolo;
-                ins_nuovo_biglietto(head,data,codice,fila,posto,prezzo);
-                ins_nuovo_spettacolo(head2,codice,titolo);
+            case '4':
                 break;
 
-            case 'H':
-                cout<<"inserisci il biglietto da cancellare"<<endl;
-                cin>>codice;
-                canc(head,codice);
-                canc2(head2,codice);
+            case '5':
                 break;
 
-            case 'L':
-                salvataggio1(head);
-                salvataggio2(head2);
+            case '6':
+                break;
+
+            case '7':
+                break;
+
+            case '8':
+                break;
+
+            case '9':
                 break;
 
             case 'X':
